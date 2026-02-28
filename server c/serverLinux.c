34655bc14d32ec7957ba58f7b4a475d0a6312c1b
Robertas <robervicsat@gmail.com>
2026-02-28 22:12:11 +0200

    server linux works

--- server c/serverLinux.c
+++ server c/serverLinux.c
@@ -1,46 +1,40 @@
-#define _WIN32_WINNT 0x0600
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-
-#pragma comment(lib, "ws2_32.lib")
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 #define PORT "20000"
 #define BUFSIZE 1024
 
 int main(void) {
-    WSADATA wsaData;
     struct addrinfo hints, *res, *p;
-    SOCKET sockfd, new_fd;
+    int sockfd, new_fd;
     char buf[BUFSIZE];
     int rv, numbytes;
-
-    // Initialize Winsock (Beej requirement)
-    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
-        fprintf(stderr, "WSAStartup failed\n");
-        exit(1);
-    }
+    int yes = 1;
 
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;     // IPv4 or IPv6
+    hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;     // use my IP
+    hints.ai_flags = AI_PASSIVE;
 
     if ((rv = getaddrinfo("::1", PORT, &hints, &res)) != 0) {
-        fprintf(stderr, "getaddrinfo: %d\n", rv);
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         exit(1);
     }
 
-    // loop through results and bind
     for (p = res; p != NULL; p = p->ai_next) {
         sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-        if (sockfd == INVALID_SOCKET) continue;
+        if (sockfd == -1) continue;
 
-        if (bind(sockfd, p->ai_addr, (int)p->ai_addrlen) == SOCKET_ERROR) {
-            closesocket(sockfd);
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
             continue;
         }
         break;
@@ -50,20 +44,22 @@ int main(void) {
 
     if (p == NULL) {
         fprintf(stderr, "server: failed to bind\n");
-        WSACleanup();
         exit(2);
     }
 
     listen(sockfd, 1);
-    printf("Serveris laukia...\n");
+    printf("Serveris laukia (Linux)...\n");
 
     new_fd = accept(sockfd, NULL, NULL);
+    if (new_fd == -1) {
+        perror("accept");
+        exit(3);
+    }
     printf("Klientas prisijunge\n");
 
     while ((numbytes = recv(new_fd, buf, BUFSIZE, 0)) > 0) {
         printf("Gauta: %.*s\n", numbytes, buf);
 
-        // uppercase like Python .upper()
         for (int i = 0; i < numbytes; i++) {
             if (buf[i] >= 'a' && buf[i] <= 'z')
                 buf[i] -= 32;
@@ -72,8 +68,8 @@ int main(void) {
         send(new_fd, buf, numbytes, 0);
     }
 
-    closesocket(new_fd);
-    closesocket(sockfd);
-    WSACleanup();
+    close(new_fd);
+    close(sockfd);
+    
     return 0;
 }
