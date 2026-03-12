26cd37832323679be1ecce8830e2be505f962860
NoSparkles <robervicsat@gmail.com>
2026-03-12 17:11:32 +0200

    speliotojas done

--- server c/Speliotojas.c
+++ server c/Speliotojas.c
@@ -14,61 +14,68 @@ void set_nonblocking(int fd) {
     fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 }
 
+// Pagalbinė funkcija nusiųsti žinutę visiems prisijungusiems
+void broadcast(int clients[], const char *msg) {
+    for (int i = 0; i < 4; i++) {
+        if (clients[i] != -1) {
+            send(clients[i], msg, strlen(msg), 0);
+        }
+    }
+}
+
 int main(void) {
     int ports[4] = {20000, 20001, 20002, 20003};
     int server_fds[4];
     int client_fds[4] = {-1, -1, -1, -1};
     int target_num = -1;
-    int current_turn = 1;
+    int current_turn = 1; 
+
+    FILE *outputFile;
 
     for (int i = 0; i < 4; i++) {
-        // 1. Sukuriam IPv6 lizdą
         server_fds[i] = socket(AF_INET6, SOCK_STREAM, 0);
-        if (server_fds[i] < 0) {
-            perror("socket");
-            exit(1);
-        }
-
-        // 2. Leidžiam greitai pakartotinai naudoti portą
         int opt = 1;
         setsockopt(server_fds[i], SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-        // 3. IŠJUNGIAM IPV6_V6ONLY (kad veiktų ir IPv4)
+        
         int v6only = 0;
-        if (setsockopt(server_fds[i], IPPROTO_IPV6, IPV6_V6ONLY, &v6only, sizeof(v6only)) < 0) {
-            perror("setsockopt v6only");
-        }
+        setsockopt(server_fds[i], IPPROTO_IPV6, IPV6_V6ONLY, &v6only, sizeof(v6only));
 
-        // 4. Bind'inam prie visų sąsajų
         struct sockaddr_in6 addr;
         memset(&addr, 0, sizeof(addr));
         addr.sin6_family = AF_INET6;
         addr.sin6_port = htons(ports[i]);
-        addr.sin6_addr = in6addr_any; // Klausosi [::] (visų IPv6 ir IPv4 adresų)
+        addr.sin6_addr = in6addr_any;
 
         if (bind(server_fds[i], (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-            printf("Klaida bind'inant portą %d: %s\n", ports[i], strerror(errno));
+            perror("bind");
             exit(1);
         }
-
         listen(server_fds[i], 5);
         set_nonblocking(server_fds[i]);
-        printf("Serveris %d (IPv4+IPv6) laukia porte %d...\n", i, ports[i]);
+        printf("Portas %d paruostas.\n", ports[i]);
     }
 
     while (1) {
+        outputFile = fopen("logs.txt", "a");
         for (int i = 0; i < 4; i++) {
-            // Bandome priimti naują ryšį
+            // Naujų ryšių priėmimas
             int new_fd = accept(server_fds[i], NULL, NULL);
             if (new_fd != -1) {
                 set_nonblocking(new_fd);
                 if (client_fds[i] != -1) close(client_fds[i]);
                 client_fds[i] = new_fd;
-                printf("Prisijungta prie porto %d\n", ports[i]);
-                send(new_fd, "Sveiki! Iveskite skaiciu.\n", 26, 0);
+                
+                if (i == 0) {
+                    send(new_fd, "Sveikas, Vadove. Ivesk skaiciu (0-10):\n", 39, 0);
+                    fprintf(outputFile, "Vadovui siusta: Sveikas, Vadove. Ivesk skaiciu (0-10):\n");
+                }
+                else {
+                    send(new_fd, "Sveikas, Zaidejau. Lauk savo eiles...\n", 38, 0);
+                    fprintf(outputFile, "Zaidejui siusta: Sveikas, Vadove. Ivesk skaiciu (0-10):\n");
+                }
             }
 
-            // Tikriname duomenis
+            // Duomenų gavimas
             if (client_fds[i] != -1) {
                 char buf[BUFSIZE];
                 int n = recv(client_fds[i], buf, BUFSIZE - 1, 0);
@@ -76,28 +83,57 @@ int main(void) {
                 if (n > 0) {
                     buf[n] = '\0';
                     int val = atoi(buf);
-                    
-                    if (i == 0) { // Vadovas
+
+                    if (i == 0) { // VADOVAS
                         target_num = val;
-                        printf("VADOVAS (20000) nustate: %d\n", target_num);
-                        for(int j=1; j<4; j++) if(client_fds[j] != -1) send(client_fds[j], "Pradedam!\n", 10, 0);
-                    } else if (target_num != -1 && i == current_turn) { // Žaidėjai
+                        current_turn = 1; // Pradeda 1 žaidėjas
+                        broadcast(client_fds, "\nZaidimas prasidejo! Vadovas nustate skaiciu.\n");
+                        fprintf(outputFile, "Visiems siusta: \nZaidimas prasidejo! Vadovas nustate skaiciu.\n");
+                        if (client_fds[current_turn] != -1) 
+                            send(client_fds[current_turn], "Tavo eile speti: ", 17, 0);
+                    } 
+                    else if (target_num != -1 && i == current_turn) { // ŽAIDĖJAI
                         if (val == target_num) {
-                            send(client_fds[i], "ATSPĖJAI!\n", 11, 0);
-                            target_num = -1;
+                            char win_msg[100];
+                            sprintf(win_msg, "\n!!! ATSPEJO !!! Zaidejas porte %d laimejo. Skaicius buvo: %d\n", ports[i], target_num);
+                            broadcast(client_fds, win_msg);
+                            fprintf(outputFile, "Zaidejams siusta: \n!!! ATSPEJO !!! Zaidejas porte %d laimejo. Skaicius buvo: %d\n", ports[i], target_num);
+                            target_num = -1; // Resetinam žaidimą
+                            broadcast(client_fds, "Laukiam kol vadovas nustatys nauja skaiciu...\n");
+                            fprintf(outputFile, "Zaidejams siusta: Laukiam kol vadovas nustatys nauja skaiciu...\n");
                         } else {
-                            send(client_fds[i], (val < target_num ? "Daugiau\n" : "Mažiau\n"), 9, 0);
-                            current_turn = (current_turn % 3) + 1;
+                            // 1. Sukuriam pranešimą visiems apie atliktą spėjimą
+                            char broadcast_guess[100];
+                            sprintf(broadcast_guess, "\nZaidejas porte %d spejo skaiciu: %d\n", ports[i], val);
+                            broadcast(client_fds, broadcast_guess);
+                            fprintf(outputFile, "Visems siusta: \nZaidejas porte %d spejo skaiciu: %d\n", ports[i], val);
+
+                            // 2. Nusiunčiam užuominą tik spėjusiam žaidėjui
+                            char hint[50];
+                            sprintf(hint, "Skaicius yra %s.\n", (val < target_num ? "DIDESNIS" : "MAZESNIS"));
+                            send(client_fds[i], hint, strlen(hint), 0);
+                            fprintf(outputFile, "Zaidejui %d siusta: \nZaidejas porte %d spejo skaiciu: %d\n", client_fds[i], ports[i], val);
+                            
+                            // 3. Eilės perdavimas (lieka toks pat)
+                            do {
+                                current_turn = (current_turn % 3) + 1;
+                            } while (client_fds[current_turn] == -1);
+
+                            send(client_fds[current_turn], "Tavo eile speti: ", 17, 0);
+                            fprintf(outputFile, "Zaidejui %d siusta: Tavo eile speti: ", client_fds[current_turn]);
                         }
+                    } else if (target_num != -1 && i != current_turn) {
+                        send(client_fds[i], "Dar ne tavo eile!\n", 18, 0);
+                        fprintf(outputFile, "Zaidejui %d siusta: Dar ne tavo eile!\n", client_fds[i]);
                     }
                 } else if (n == 0 || (n < 0 && errno != EAGAIN && errno != EWOULDBLOCK)) {
-                    // Klientas atsijungė arba įvyko klaida
                     close(client_fds[i]);
                     client_fds[i] = -1;
                 }
             }
         }
         usleep(10000); 
+        fclose(outputFile);
     }
     return 0;
 }
